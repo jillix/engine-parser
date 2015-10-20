f274e037061ad62b52da631b09b7620a24637dc4
Ionică Bizău <bizauionica@gmail.com>
2015-10-20 17:00:42 +0300

    Playing with the renameInstance

--- example/index.js
+++ example/index.js
@@ -3,15 +3,15 @@ var EngineParserGen = require("../lib");
 
 // "test-app" should be a valid Engine app in $ENGINE_APPS dir
 var epg = new EngineParserGen("test-app");
-epg.parse(function (err, parsed) {
-    debugger
-});
-
-//epg.renameInstance("A", "B", function (err) {
-//    if (err) {
-//        return console.log(err);
-//    }
-//    epg.save(function (err) {
-//        console.log(err || "Saved");
-//    });
+//epg.parse(function (err, parsed) {
+//    debugger
 //});
+
+epg.renameInstance("A", "B", (err) => {
+    if (err) {
+        return console.log(err);
+    }
+    epg.save(function (err) {
+        console.log(err || "Saved");
+    });
+});
