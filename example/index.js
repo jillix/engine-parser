c0e145482a0de99613a96e0b6719645edd38ad2c
Ionică Bizău <bizauionica@gmail.com>
2015-12-07 16:30:11 +0200

    Playing with the examples

--- example/index.js
+++ example/index.js
@@ -4,23 +4,19 @@ var EngineParser = require("../lib");
 // Initialize the parser-generator
 var ep = new EngineParser(`${__dirname}/engine-test`);
 
-ep.parse((err, data) => {
-    console.log(err || data);
-});
-
 // Rename layout->another_layout
-//epg.renameInstance("layout", "another_layout", (err, toBeSaved, toBeDeleted) => {
-//    if (err) { return console.log(err); }
-//
-//    // Save the changes on the disk
-//    epg.save({
-//
-//        // Delete these ones
-//        delete: toBeDeleted
-//
-//        // Override these ones
-//      , save: toBeSaved
-//    }, function (err) {
-//        console.log(err || "Saved");
-//    });
-//});
+epg.renameInstance("layout", "another_layout", (err, toBeSaved, toBeDeleted) => {
+    if (err) { return console.log(err); }
+
+    // Save the changes on the disk
+    epg.save({
+
+        // Delete these ones
+        delete: toBeDeleted
+
+        // Override these ones
+      , save: toBeSaved
+    }, function (err) {
+        console.log(err || "Saved");
+    });
+});
