e09b00e43f544accd77433dc5e7a0107db4be286
Ionică Bizău <bizauionica@gmail.com>
2015-12-08 13:11:46 +0200

    Save the things to disk

--- example/index.js
+++ example/index.js
@@ -4,19 +4,25 @@ var EngineParser = require("../lib");
 // Initialize the parser-generator
 var ep = new EngineParser(`${__dirname}/engine-test`);
 
-// Rename layout->another_layout
-epg.renameInstance("layout", "another_layout", (err, toBeSaved, toBeDeleted) => {
+// Parse the input json
+ep.parse(err => {
     if (err) { return console.log(err); }
 
-    // Save the changes on the disk
-    epg.save({
+    // Rename layout->another_layout
+    ep.renameInstance("layout", "another_layout", (err, toBeSaved, toBeDeleted) => {
+        if (err) { return console.log(err); }
 
-        // Delete these ones
-        delete: toBeDeleted
+        // Save the changes on the disk
+        ep.save({
 
-        // Override these ones
-      , save: toBeSaved
-    }, function (err) {
-        console.log(err || "Saved");
+            // Delete these ones
+            delete: toBeDeleted
+
+            // Override these ones
+          , save: toBeSaved
+        }, function (err) {
+            console.log(err || "Saved");
+        });
     });
 });
+
